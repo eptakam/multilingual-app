05577df26f3836182b0b777fce3bbe9f92257d21
eptakam <emmataks@gmail.com>
2024-10-18 18:54:53 -0300

    Configuring rounting and replaced 'module.exports' by 'export default' in the next-intl files config

--- next-intl.config.js
+++ next-intl.config.js
@@ -1,6 +1,13 @@
-// next-intl.config.js
-module.exports = {
+const config = {
   locales: ["en", "fr", "wo"], // add more languages here
   defaultLocale: "en",
   namespace: ["common", "frontend"], // add more namespaces here
 };
+
+export default config;
+
+// module.exports = {
+//   locales: ["en", "fr", "wo"], // add more languages here
+//   defaultLocale: "en",
+//   namespace: ["common", "frontend"], // add more namespaces here
+// };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
+  rewrites: async () => {
+    const { nextI18NextRewrites } = await import("next-intl/plugin");
+    return nextI18NextRewrites();
+  },
+};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+// const { nextI18NextRewrites } = require("next-intl/plugin");
+
+// module.exports = {
+//   i18n: {
+//     locales: ["en", "fr"],
+//     defaultLocale: "en",
+//   },
+//   rewrites: async () => nextI18NextRewrites(),
+// };
